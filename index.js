4d37c2ef1d26f1b1cdcd27e7196b7c515b72049f
Christian Sanz <chrissanz@gmail.com>
2011-06-05 17:01:34 -0700

    updated port

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ require.paths = require.paths.unshift(__dirname + '/../node_modules');
 
 var app = require('./lib/boot.js')();
 
-app.listen(4000);
+
+var port = process.env.PORT || 3000;
+console.log("Listening on " + port);
+
+app.listen(port);
